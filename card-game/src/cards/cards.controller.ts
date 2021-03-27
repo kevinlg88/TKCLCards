7c56065608cfc2b1182b227c90b982adc228a681
Kevin Grandez <kevinlg08@gmail.com>
2021-03-27 00:24:04 -0300

    Crud cards em memoria

--- card-game/src/cards/cards.controller.ts
+++ card-game/src/cards/cards.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Card } from './shared/card';
+import { CardService } from './shared/card.service';
 
 @Controller('cards')
-export class CardsController {}
+export class CardsController 
+{
+    constructor(private cardService: CardService) {}
+
+    @Get()
+    async getAll(): Promise<Card[]> 
+    {
+        return this.cardService.getAll();
+    }
+
+    @Get(':id')
+    async getById(@Param('id') id: string): Promise<Card> 
+    {
+        return this.cardService.getById(id);
+    }
+
+    @Post()
+    async create(@Body() card: Card): Promise<Card>
+    {
+        return this.cardService.create(card);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id:string, @Body() card:Card):Promise<Card>
+    {
+        card.id = id;
+        return this.cardService.update(card);
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id:string)
+    {
+        this.cardService.delete(id);
+    }
+}
--- card-game/src/cards/shared/card.service.ts
+++ card-game/src/cards/shared/card.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { Card } from './card';
 
 @Injectable()
-export class CardService {}
+export class CardService 
+{
+    cards: Card[]= [
+        {id: "003",uuid: "a9099a45-3334-9e55-7b14-0561e350770c" },
+        {id:"004", uuid:"03acb46b-6a69-3c63-e14d-30d0f94fa161"},
+        {id:"005", uuid:"092bdcf2-eb25-6672-5b1c-2b9a079e6fb9"},
+        {id:"006", uuid:"68cee3e7-50f3-9eaf-3771-86b7061d1ba6"},
+        {id:"007", uuid:"cf204413-9e80-29de-69cc-eea68e4b060d"},
+        {id:"008", uuid:"36e79db0-0f32-05ab-32d5-3d4f48049773"},
+        {id:"009", uuid:"e84cdf18-508c-acf0-16eb-71399e59f835"},
+        {id:"010", uuid:"0ef20532-b9de-baee-36e8-7d989b345fa7"},
+        {id:"011", uuid:"91becf90-df80-e7f4-b60b-abd6f2f7b38b"},
+        {id:"012", uuid:"b0ac392b-e799-1583-844a-900866321bfe"},
+        
+    ];
+
+    getAll()
+    {
+        return this.cards;
+    }
+
+    getById(id:string)
+    {
+        const card = this.cards.find((value) => value.id == id);
+        return card;
+    }
+
+    create(card:Card)
+    {
+        this.cards.push(card);
+        return card;
+    }
+
+    update(card:Card)
+    {
+        const cardArray = this.getById(card.id);
+        if(cardArray)
+        {
+            cardArray.uuid = card.uuid;
+        }
+
+        return cardArray;
+    }
+
+    delete(id:string)
+    {
+        const index = this.cards.findIndex((value) => value.id == id);
+        this.cards.splice(index, 1);
+    }
+}
